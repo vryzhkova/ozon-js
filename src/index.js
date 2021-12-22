c91d8eaae98f9961cb92f3745b930f3a5444a91b
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-12-22 14:45:45 +0300

    lesson 4: done filter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import cart from "./modules/cart";
 import load from "./modules/load";
 import search from "./modules/search";
-import catalog from "./modules/catalog"
+import catalog from "./modules/catalog";
+import filter from "./modules/filter";
+import renderCart from "./modules/renderCart"
 
 cart();
 load();
 search();
 catalog();
+filter();
+renderCart();
--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,10 +1,28 @@
+import renderCart from "./renderCart";
+import postData from "./postData"
+
 const cart = () => {
     const cartBtn = document.getElementById('cart');
     const cartModal = document.querySelector('.cart');
     const cartCloseBtn = document.querySelector('.cart-close');
+    const goodsWrapper = document.querySelector('.goods');
+    const cartTotal = cartModal.querySelector('.cart-total > span');
+    const cartWrapper = document.querySelector('.cart-twrapper');
+    const cartSendBtn = cartModal.querySelector('.cart-confirm')
+    
+
+    console.log(cartTotal);
 
     const openCart = () => {
+        const cart = localStorage.getItem('cart') ? 
+        JSON.parse(localStorage.getItem('cart')) : [];
+
         cartModal.style.display = 'flex';
+
+        renderCart(cart);
+        cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price
+        }, 0)
     }
 
     const closeCart = () => {
@@ -12,8 +30,60 @@ const cart = () => {
     }
 
     cartBtn.addEventListener('click', openCart)
+    cartCloseBtn.onclick = ('click', closeCart);
 
-    cartCloseBtn.onclick = closeCart;
+    goodsWrapper.addEventListener('click', (event) => {
+        if(event.target.classList.contains('btn-primary')) {
+            const card = event.target.closest('.card');
+            const key = card.dataset.key;
+            const goods = JSON.parse(localStorage.getItem('goods'));
+            const cart = localStorage.getItem('cart') ? 
+            JSON.parse(localStorage.getItem('cart')) : [];
+
+            const goodItem = goods.find((item) => {
+                return item.id === +key;
+            })
+
+            cart.push(goodItem);
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+        };
+    })
+
+    cartWrapper.addEventListener('click', (event) => {
+        if(event.target.classList.contains('btn-primary')) {
+            const cart = localStorage.getItem('cart') ? 
+            JSON.parse(localStorage.getItem('cart')) : [];
+            const card = event.target.closest('.card');
+            const key = card.dataset.key;
+            const index = cart.findIndex((item) => {
+                return item.id === +key;
+            })
+
+            cart.splice(index, 1)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+
+            renderCart(cart);
+
+            cartTotal.textContent = cart.reduce((sum, goodItem) => {
+                return sum + goodItem.price
+            }, 0)
+        }
+    })
+
+    cartSendBtn.addEventListener('click', () => {
+        const cart = localStorage.getItem('cart') ? 
+        JSON.parse(localStorage.getItem('cart')) : [];
+
+        postData().then(() => {
+            localStorage.removeItem('cart')
+
+            renderCart([])
+
+            cartTotal.textContent = 0;
+        })
+    })
 }
 
 export default cart;
